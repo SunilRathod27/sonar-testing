5f42d0facfa851e5f24ac0e9ff52dae5aa0f1b4b
Sunil Rathod <sunilr@alitersolutions.com>
2023-09-01 15:00:20 +0530

    changes

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ const http = require('http');
 
 // Create the server
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Server is running!');
+  if (req.method === 'GET') {
+    if (req.url === '/api') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      const responseData = {
+        message: 'This is a GET API response',
+        timestamp: new Date().toISOString()
+      };
+      res.end(JSON.stringify(responseData));
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not Found');
+    }
+  } else {
+    res.statusCode = 405;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Method Not Allowed');
+  }
 });
 
 // Start the server
 function startServer() {
   server.listen(3000, 'localhost', () => {
-    console.log('Server started on port 5000');
+    console.log('Server started on port 3000');
   });
 }
 
